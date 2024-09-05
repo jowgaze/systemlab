2c736bade537ea697b9c3111eb30401651409039
jowgaze <jonatha.biomon.hp@gmail.com>
2024-09-05 16:27:22 -0300

    initial commit

---
+++ src/main/java/com/systemlab/SystemlabApplication.java
@@ -0,0 +1,13 @@
+package com.systemlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systemlab/SystemlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
